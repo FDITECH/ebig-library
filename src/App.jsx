9604d7d555c9ec267da803877b094ee0c02a7e17
thuntmac <thunt@wini.vn>
2026-06-10 14:32:04 +0700

    rubytext ebigeditor

--- src/App.jsx
+++ src/App.jsx
@@ -504,9 +504,9 @@ export default function App() {
       <section style={sectionStyle}>
         <div style={headingStyle}>EbigEditor</div>
         <EbigEditor
-          initValue='<p><span style="font-family:&quot;Time New Roman&quot;,serif;font-size:12.0pt;"><span style="line-height:115%;"><ruby style="ruby-align:space-around;"><span style="font-family:&quot;MS Mincho&quot;;" lang="JA">受付</span><rp>(</rp><rt style="font-family:&quot;MS Mincho&quot;;font-size:6.0pt;layout-grid-mode:line;">うけつけ</rt><rp>)</rp></ruby><ruby style="ruby-align:space-around;"><span style="font-family:&quot;MS Mincho&quot;;" lang="JA">山田</span><rp>(</rp><rt style="font-family:&quot;MS Mincho&quot;;font-size:6.0pt;layout-grid-mode:line;">やまだ</rt><rp>)</rp></ruby></span></span><span style="font-family:&quot;UD Digi Kyokasho NK-R&quot;,serif;font-size:15.0pt;"><span lang="JA" dir="ltr">躾とは、ルールを守る習慣を身につけることである。</span></span></p>'
+          initValue={ckValue}
           placeholder="Type something with rich-text support…"
-          style={{ width: '100%', minHeight: '12rem' }}
+          style={{ width: '100%', minHeight: '12rem', fontSize: 48, lineHeight: "normal" }}
           onChange={(value) => console.log('Editor value:', value)}
         />
       </section>
