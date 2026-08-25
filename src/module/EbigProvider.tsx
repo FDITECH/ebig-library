8d2f3398c3771f3321b943524f8b42448288b120
thuntmac <thunt@wini.vn>
2026-08-25 14:10:45 +0700

    0.1.29

--- src/module/EbigProvider.tsx
+++ src/module/EbigProvider.tsx
@@ -381,7 +381,7 @@ export const useEbigContext = () => {
     return context;
 }
 
-const href = "https://cdn.ebig.co/library/style/v0.1.15/"
+const href = "https://cdn.ebig.co/library/style/v0.1.28/"
 const appendStyleSheet = () => {
     const tmp = document.createElement("div")
     tmp.innerHTML = `
