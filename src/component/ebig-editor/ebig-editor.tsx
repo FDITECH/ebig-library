481536f272a417265cb0ad44a115741c905f7260
thuntmac <thunt@wini.vn>
2026-08-28 12:44:48 +0700

    ....

--- src/component/ebig-editor/ebig-editor.tsx
+++ src/component/ebig-editor/ebig-editor.tsx
@@ -362,9 +362,6 @@ export const EbigEditor = forwardRef<RefProps, Props>(({ id, onChange, onBlur, d
             onClick={(disabled || readOnly) ? undefined : ((ev: any) => {
                 if (isOpenEmoji) return null;
                 const rect = ev.target.closest("div").getBoundingClientRect()
-                const tmp = document.createElement("div")
-                tmp.style.position = "fixed"
-                ev.currentTarget.after(tmp)
                 let offset: any = { left: rect.x, top: rect.bottom + 1 }
                 if (offset.left + 268 >= document.body.offsetWidth) {
                     delete offset.left
